5752a0e39ec46f3503db6b771ba8a17fb07bf4a4
unknown <shivkumar.napit@DMMB-PBV9CFA.global.trafigura.com>
2015-02-06 20:30:33 +0530

    added comment for Project POC

--- src/com/shiv/loginapp/service/LoginService.java
+++ src/com/shiv/loginapp/service/LoginService.java
@@ -2,6 +2,7 @@ package com.shiv.loginapp.service;
 
 public class LoginService {
 	
+	/* POC first*/
 	// Model in this application is the Boolean that we are passing from service to the controller
 	public boolean authenticate(String userId, String password){
 		
